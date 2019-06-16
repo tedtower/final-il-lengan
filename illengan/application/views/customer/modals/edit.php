5f5b87a80be6062093282eec1cff8869ad2fab10
Carla <34297873+Sherlea@users.noreply.github.com>
2019-06-17 05:56:37 +0800

    Updated customer module

--- illengan/application/views/customer/modals/edit.php
+++ illengan/application/views/customer/modals/edit.php
@@ -1,5 +1,5 @@
 <!-- Edit Modal -->
-    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editOrderModal" aria-hidden="true">
+<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editOrderModal" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content" style="padding:0px;">
             <!-- Modal Body -->
@@ -10,6 +10,7 @@
                 <h3 class="text-center gab mt-2" id="edit_name"></h3>
                 <div class="md-form input-group mb-3">
                     <div class="d-flex flex-row align-items-center">
+                        <span id="edit_row" hidden></span>
                         <span class="pr-2">Quantity:</span>
                         <div class="input-group-prepend">
                             <button class="btn btn-md btn-light m-0 px-3 py-2 z-depth-0" type="button" id="qty-minus">
@@ -23,25 +24,19 @@
                     </div>
                 </div>
                 <!--PREFERENCE-->
-                <div class="input-group w-auto mb-3 d-flex align-items-center">
-                    <label>Preference: &nbsp;</label>
-                    <select name="edit_pref" id="edit_pref" class="form-control"></select>
-                </div>
-                <!--IF ORDERLIST HAS ADDONS OR MENU HAS ADDONS-->
-                <button class="btn btn-md btn-outline-mdb-color m-0 px-3 py-2 mb-2 z-depth-0" type="button">Add Addons</button>
-                <div class="input-group mb-3 delius d-flex align-items-center">
-                    <select class="browser-default custom-select" id="edit_addons" name="edit_addons[]"></select>
-                    <input type="number" min="1" placeholder="Quantity..." aria-label="Add-on Quantity" class="form-control" id="edit-qty">
-                    <a href="javascript:void(0)" class="text-danger ml-1 px-2" onclick="$(this).parent().remove();"><i class="fal fa-times"></i></a>
-                </div>
+                <div id="edit_preferencable" class="input-group w-auto mb-3 d-flex align-items-center"></div>
+                <!--ADDONS BUTTON-->
+                <div id="edaddbutt"><button class="btn btn-md btn-outline-mdb-color m-0 px-3 py-2 mb-2 z-depth-0" id="addOnButt" type="button">Add Addons</button></div>
+                <!--ADDONS-->
+                <div id="edit_addonable" class="mb-2"></div>
                 <!--REMARKS-->
                 <div class="d-flex flex-row">
-                    <span class="px-1 mt-1 label-indent">Remarks: </span>
-                    <textarea class="form-control" id="exampleFormControlTextarea5" rows="2"></textarea>
+                    <span class="px-1 label-indent">Notes: </span>
+                    <textarea class="form-control" id="edit_remarks" rows="2"></textarea>
                 </div>
             </div>
-            <div class="modal-footer py-1 justify-content-center">
-                <button type="button" class="btn btn-mdb-color" href="#proceed_order">Save</button>
+            <div class="mb-2 py-1 justify-content-center text-center">
+                <button type="button" class="btn btn-mdb-color" id="edit_orderlist">Save</button>
             </div>
             </div>
         </div>
