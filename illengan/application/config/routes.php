fd3fef1c66dbbd1e22a05b33d38bf9a17bcf7eae
Carla <34297873+Sherlea@users.noreply.github.com>
2019-07-11 21:31:01 +0800

    Update routes.php

--- illengan/application/config/routes.php
+++ illengan/application/config/routes.php
@@ -60,7 +60,8 @@ $route['admin/officialreceipt/formedit'] = "adminview/viewORFormEdit";
 $route['admin/consumption/formadd'] = "adminview/viewConsumptionFormAdd";
 $route['admin/stock/spoilage/formadd'] = "adminview/viewSpoilagesStockAdd";
 $route['admin/addons/spoilage/formadd'] = "adminview/viewSpoilagesAddonAdd";
-
+$route['admin/stocks/loadDataStocks/(:num)'] = "adminview/loadDataStocks/$1";
+$route['admin/menu/loadDataCategories/(:num)'] = "adminview/loadDataCategories/$1";
 //End Viewing Routes
 
 //Not Sure Routes
