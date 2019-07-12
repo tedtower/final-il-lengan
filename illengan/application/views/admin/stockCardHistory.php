3a9572960144bde9d5e7733ab92d0ed4efd20ca6
Marvin <marvinolivas3@gmail.com>
2019-07-12 19:42:51 +0800

    stock card history

--- illengan/application/views/admin/stockCardHistory.php
+++ illengan/application/views/admin/stockCardHistory.php
@@ -10,28 +10,23 @@
                         <p style="text-align:right; font-weight: regular; font-size: 16px;float:right">
                             <?php echo date("M j, Y -l"); ?>
                         </p>
+                        <button class="btn btn-primary btn-sm m-0" data-toggle="modal" data-target="#addReport"><i class="fas fa-filter"></i> Filter</button>
                     </div>
-                    <!--Card--> 
                     <div class="card" style="background:whitesmoke">
                         <div class="card-body">
-                                <div class="mb-4"><b>Stock Item:</b></div>
-                                <div class="form-row">
-                                    <div class="input-group mb-3 col">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="inputGroup-sizing-sm" style="width:100px;background:rgb(242, 242, 242);color:rgba(48, 46, 46, 0.9);font-size:14px;">
-                                                Start Date</span>
-                                        </div>
-                                        <input class="form-control form-control-sm" name="sDate" type="date" class="no-border"  data-validate="required" message="Start date is required!" required>
-                                    </div>
-                                    <div class="input-group mb-3 col">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="inputGroup-sizing-sm" style="width:100px;background:rgb(242, 242, 242);color:rgba(48, 46, 46, 0.9);font-size:14px;">
-                                                End Date</span>
-                                        </div>
-                                        <input class="form-control form-control-sm" name="eDate" type="date" class="no-border"  data-validate="required" message="End date is required!" required>
-                                        <button class="btn btn-sm" style="background:#227C57;color:white;width:10%;border:0"><b>Ok</b></button>
-                                    </div>
-                                </div>
+                        <div style="width:100%;overflow:auto;">
+                            <div style="overflow:auto;">
+                            <span style="float:left;width:40%;"><b>Stock Item:</b> <?= $stock['stName'] . " " . $stock['stSize']?></span>
+                                <span style="float:left;width:40%"><b>Beginning Inventory Date:</b> <?= $logs[0]['logDate']?></span>
+                                <span style="float:left;width:20%"><b>Beginning Qty:</b> <?= $logs[0]['actual'] . " " . $stock['uomAbbreviation']?></span>
+                            </div>
+                            
+                            <div style="overflow:auto;">
+                                <span style="float:left;width:40%"><b>Storage:</b> <?= $stock['stLocation']?></span>
+                                <span style="float:left;width:40%"><b>Category:</b> <?= $stock['ctName']?></span>
+                                <span style="float:left;width:20%"><b>Status:</b> <?= $stock['stStatus']?></span>
+                            </div>
+                        </div>
                         </div>
                     </div>
                     <!--Table--> 
@@ -47,25 +42,83 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td><img src="/assets/media/admin/plus.png" style="height:18px;width:18px"/></td>
-                                <td>delivery</td>
-                                <td>12345356356</td>
-                                <td>Janiuary 10 , 2019</td>
-                                <td>10</td>
-                                <td>25</td>
-                            </tr>
-                            <!--table row when an inventory check was performed-->
-                            <tr style="background:whitesmoke">
-                                <td><img src="/assets/media/admin/check.png" style="height:18px;width:18px"/></td>
-                                <td>Inventory Check</td>
-                                <td><b>Date:</b> </td>
-                                <td><b>Physical Count:</b> </td>
-                                <td><b>Discrepancy:</b> </td>
-                                <td><b>Remarks:</b> </td>
-                            </tr>
+                        <?php if(!empty($logs)){
+                            $icon;
+                            foreach($logs as $log){
+                                switch($log['type']){
+                                    case 'restock':
+                                        echo '<tr>
+                                                <td><img src="/assets/media/admin/plus.png" style="height:18px;width:18px"/></td>
+                                                <td>Restock</td>
+                                                <td>'.$log['tID'].'</td>
+                                                <td>'. $log['logDate'].'</td>
+                                                <td>'. $log['actual'].'</td>
+                                                <td>'. $log['remain'].'</td>
+                                            </tr>';
+                                        break;
+                                    case 'beginning':
+                                        echo '<tr style="background:#fcfcfc">
+                                                <td><img src="/assets/media/admin/check.png" style="height:18px;width:18px;"/></td>
+                                                <td>Beginning</td>
+                                                <td><b>Date:</b> '.$log['logDate'].'</td>
+                                                <td><b>Physical Count:</b> '.$log['actual'].' </td>
+                                                <td><b>Discrepancy:</b> '.$log['discrepancy'].' </td>
+                                                <td><b>Remarks:</b> '.$log['tiRemarks'].'</td>
+                                            </tr>';
+                                        break;
+                                    default:
+                                        echo '<tr>
+                                                <td><img src="/assets/media/admin/negative.png" style="height:18px;width:18px"/></td>
+                                                <td>'.ucwords($log['type']).'</td>
+                                                <td>'.$log['tiID'].'</td>
+                                                <td>'. $log['logDate'].'</td>
+                                                <td>'. $log['actual'].'</td>
+                                                <td>'. $log['remain'].'</td>
+                                            </tr>';
+                                        break;
+                                }
+                            }
+                        }
+                        ?>
                         </tbody>
                     </table>
+                    <!--Start of Add Report Modal-->
+                    <div class="modal fade" id="addReport" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLongTitle">Filter by Date</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <form action="<?php echo base_url()?>admin/stocklog/history/filter" method="post" accept-charset="utf-8">
+                                    <div class="modal-body">
+                                        <p>Please input the date for the following:</p>
+                                        <input type="hidden" name="stID" value="<?= $stock['stID'];?>" class="form-control form-control-sm">
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text" id="inputGroup-sizing-sm" style="width:100px;background:rgb(242, 242, 242);color:rgba(48, 46, 46, 0.9);font-size:14px;">
+                                                    Start Date</span>
+                                            </div>
+                                            <input class="form-control form-control-sm" name="sDate" type="date" class="no-border"  data-validate="required" message="Start date is required!" required>
+                                        </div>
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text" id="inputGroup-sizing-sm" style="width:100px;background:rgb(242, 242, 242);color:rgba(48, 46, 46, 0.9);font-size:14px;">
+                                                    End Date</span>
+                                            </div>
+                                            <input class="form-control form-control-sm" name="eDate" type="date" class="no-border"  data-validate="required" message="End date is required!" required>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Close</button>
+                                        <button type="submit" class="btn btn-success btn-sm">Create</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
