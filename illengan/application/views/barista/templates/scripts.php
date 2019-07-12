dc54260d574cda5144bb1d624514ad0933d08d12
ambo15 <ambomelissa14@gmail.com>
2019-07-13 04:29:56 +0800

    ordereslip horizontal scrolling and checkbox validation

--- illengan/application/views/barista/templates/scripts.php
+++ illengan/application/views/barista/templates/scripts.php
@@ -1,3 +1,6 @@
 <script type="text/javascript" src="<?php echo base_url().'assets/js/chef/jquery-3.2.1.js'?>"></script>
 <script type="text/javascript" src="<?php echo base_url().'assets/js/chef/bootstrap.js'?>"></script>
 <script type="text/javascript" src="<?php echo base_url().'assets/js/chef/nav.js'?>"></script>
+<!-- <script type="text/javascript" src="<//?php echo base_url().'assets/js/barista/jquery.mousewheel.min.js'?>"></script> -->
+<!-- <script type="text/javascript" src="<//?php echo base_url().'assets/js/barista/jquery.nicescroll.min.js'?>"></script> -->
+<script type="text/javascript" src="<?php echo base_url().'assets/js/barista/jquery.nicescroll.js'?>"></script>
