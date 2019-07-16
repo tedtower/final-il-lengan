98e56c7ae271deb6369233aadd72be5adb73b287
Carla <34297873+Sherlea@users.noreply.github.com>
2019-07-16 22:16:19 +0800

    Update routes.php

--- illengan/application/config/routes.php
+++ illengan/application/config/routes.php
@@ -79,6 +79,7 @@ $route['admin/log/activity/(:num)'] = "adminview/loadActLogData/$1";
 $route['admin/loadDataSpoilagesStock/(:num)'] = "adminview/loadDataSpoilagesStock/$1";
 $route['admin/loadConsumptionData/(:num)'] = "adminview/loadConsumptionData/$1";
 $route['admin/loadDataSales/(:num)'] = "adminview/loadDataSales/$1";
+$route['admin/loadDataPurchaseOrder/(:num)'] = "adminview/loadDataPurchaseOrder/$1";
 //End Viewing Routes 
 
 //Not Sure Routes
