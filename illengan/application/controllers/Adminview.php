cca8a11ef344a091c1ef58734a4ad2151a34c7ae
Carla <34297873+Sherlea@users.noreply.github.com>
2019-07-16 22:17:39 +0800

    Update Adminview.php

--- illengan/application/controllers/Adminview.php
+++ illengan/application/controllers/Adminview.php
@@ -1130,17 +1130,46 @@ function getStockItem(){
         echo json_encode($data);		
     }
     
-    function viewPurchaseOrder(){
+     function viewPurchaseOrder(){
         if($this->checkIfLoggedIn()){
             $data['title'] = "Purchase Order";
             $this->load->view('admin/templates/head', $data);
             $this->load->view('admin/templates/sideNav');
-            $data['pos'] = $this->adminmodel->get_purchaseOrders();
-            $data['poitems'] = $this->adminmodel->get_purchaseOrderItems();
+            // $data['pos'] = $this->adminmodel->get_purchaseOrders();
+            // $data['poitems'] = $this->adminmodel->get_purchaseOrderItems();
             $this->load->view('admin/adminPurchaseOrder', $data);
         }else{
             redirect('login');
         }
+    }
+    function loadDataPurchaseOrder($record=0) {
+        $recordPerPage = 10;
+        if($record != 0){
+            $record = ($record-1) * $recordPerPage;
+        }      	
+        $recordCount = $this->adminmodel->countPurchOrd();
+        $poRecord = $this->adminmodel->get_purchaseOrdersData($record,$recordPerPage);
+        $config['base_url'] = base_url().'admin/loadDataPurchaseOrder';
+        $config['full_tag_open'] = '<ul class="pagination">';
+        $config['full_tag_close'] = '</ul>';
+        $config['last_tag_open'] = '<li class="page-link">';
+        $config['last_tag_close'] = '</li>';
+        $config['first_tag_open'] = '<li class="page-link">';
+        $config['first_tag_close'] = '</li>';
+        $config['num_tag_open'] = '<li class="page-link">&nbsp;';
+        $config['num_tag_close'] = '&nbsp;</li>';
+        $config['cur_tag_open'] = '<li class="page-link" style="background-color:#EBEEEE;width:30px;padding:7px 10px 7px 10px;font-weight:700">';
+        $config['cur_tag_close'] = '</li>';
+        $config['use_page_numbers'] = TRUE;
+        $config['next_link'] = '<li class="page-link">Next <i class="fa fa-long-arrow-right"></i></li>';
+        $config['prev_link'] = '<li class="page-link"><i class="fa fa-long-arrow-left"></i> Previous</li>';    
+        $config['total_rows'] = $recordCount;
+        $config['per_page'] = $recordPerPage;
+        $this->pagination->initialize($config);
+        $data['pagination'] = $this->pagination->create_links();
+        $data['purOrd'] = $poRecord;
+        $data['poitems'] = $this->adminmodel->get_purchaseOrderItems();
+        echo json_encode($data);		
     }
     function viewDeliveryReceipt(){
         if($this->checkIfLoggedIn()){
